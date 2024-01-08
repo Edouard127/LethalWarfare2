645cd8b5a05a829787e926fdb8b11808cfdcacfb
Kamigen <46357922+Edouard127@users.noreply.github.com>
2024-01-07 19:27:38 -0500

    Added models

--- Modules/Model/BodyReplacements.cs
+++ Modules/Model/BodyReplacements.cs
@@ -10,18 +10,21 @@ namespace LethalWarfare2.Modules.Model
         {
             return Assets.GetAssetFromName<GameObject>("Ghost Nightwar");
         }
-        /*protected override void OnEmoteStart(int emoteId)
+    }
+
+    public class AlexReplacement : BodyReplacementBase
+    {
+        protected override GameObject LoadAssetsAndReturnModel()
         {
-            if (emoteId == 1)
-            {
-                var clip = Assets.engineerE1.GetRandomAudio(StartOfRound.Instance.randomMapSeed);
-                controller.movementAudio.PlayOneShot(clip, 0.8f);
-            }
-            if (emoteId == 2)
-            {
-                var clip = Assets.engineerE2.GetRandomAudio(StartOfRound.Instance.randomMapSeed);
-                controller.movementAudio.PlayOneShot(clip, 0.8f);
-            }
-        }*/
+            return Assets.GetAssetFromName<GameObject>("Alex Keller");
+        }
+    }
+
+    public class ValeriaReplacement : BodyReplacementBase
+    {
+        protected override GameObject LoadAssetsAndReturnModel()
+        {
+            return Assets.GetAssetFromName<GameObject>("Valeria Shadow");
+        }
     }
 }
